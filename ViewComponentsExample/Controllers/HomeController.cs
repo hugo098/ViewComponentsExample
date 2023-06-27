236e5744459724375c0bd771fc2c5962b0a9fa29
ACER <hi.rodriguez1998@gmail.com>
2023-06-27 11:33:33 -0400

    Add project files.

---
+++ ViewComponentsExample/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ViewComponentsExample.Models;
+
+namespace ViewComponentsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View("Home");
+        }
+
+        [Route("about")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [Route("MyViewComponent")]
+        public IActionResult MyViewComponent()
+        {
+            return ViewComponent("Grid");
+        }
+
+        [Route("friends-list")]
+        public IActionResult LoadFriendList()
+        {
+            PersonGridModel personGridModel = new PersonGridModel()
+            {
+                GridTitle = "Persons List",
+                Persons = new List<Person>()
+                {
+                    new Person() {PersonName = "Hugo", JobTitle = "BE"},
+                    new Person() {PersonName = "Ivan", JobTitle = "FE"},
+                    new Person() {PersonName = "Hugo Ivan", JobTitle = "FS"}
+                }
+            };
+
+            return ViewComponent("Grid", new { grid = personGridModel });
+        }
+    }
+}
---
+++ ViewComponentsExample/ViewComponents/GridViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ViewComponentsExample.Models;
+
+namespace ViewComponentsExample.ViewComponents
+{
+    public class GridViewComponent: ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(PersonGridModel grid)
+        {
+            /*PersonGridModel personGridModel = new PersonGridModel()
+            {
+                GridTitle = "Persons List",
+                Persons = new List<Person>()
+                {
+                    new Person() {PersonName = "Hugo", JobTitle = "BE"},
+                    new Person() {PersonName = "Ivan", JobTitle = "FE"},
+                    new Person() {PersonName = "Hugo Ivan", JobTitle = "FS"}
+                }
+            };*/
+            await Task.Delay(1000);
+            //ViewBag.Grid = model;
+            return View("Default", grid); //invoked a partial view Views/Shared/Components/Grid/Default.cshtml
+            
+        }
+
+
+    }
+}
